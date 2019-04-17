83bbb54029863d418363251c3e7bfd009e16d7ed
chillinhen <c.hillen@gmx.net>
2019-04-17 21:08:39 +0200

    zwischenstand

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,32 @@
 (function($) {
   'use strict';
   //console.log("hello");
-  //tweak some heights
-  $('.box-social').each(function() {
-    var boxWidth = $(this).width();
-    console.log(boxWidth);
-    $(this).css("height", boxWidth);
-  });
-  var carouselHeight = $('.nav-main-cnt').height();
-  $('.flexslider.carousel').css('height',carouselHeight-16);
+  //some tweaks for smaller windows
+  function checkSize() {
+          if ($(".off-canvas").css("display") === "block") {
+            $('.flexslider.carousel').css('height','30vH');      //something
+          } else {
+              //tweak some heights
+            $('.box-social').each(function() {
+              var boxWidth = $(this).width();
+              //console.log(boxWidth);
+              $(this).css("height", boxWidth);
+            });
+            var carouselHeight = $('.nav-main-cnt').height();
+            $('.flexslider.carousel').css('height',carouselHeight-16);
+          }
+      }
+  // run test on initial page load (and put firstly in document)
+    checkSize();
+    // run test on resize of the window
+    $(window).resize(checkSize);
 
-  $(document).ready(function() {
+    //initialize flexslider
     $('.flexslider').css('display','block');
     $('.flexslider').flexslider({
       animation: "slide"
     });
     $('.flex-control-nav a, .flex-next, .flex-prev').wrapInner('<span></span>');
-  });
 
 
 })(jQuery);
